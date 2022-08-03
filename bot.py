b9a423646b5ab729897a35094032794fa393c774
Rushi Bhut <64154084+RushiBhut@users.noreply.github.com>
2022-08-03 17:55:15 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,14 +4,18 @@ import scraper
 
 client = commands.Bot(command_prefix="$")
 
+# Sends a message to the terminal when the bot is running
 @client.event
 async def on_ready():
     print("Bot is ready")
 
+# This will display the commands that are available to use by the members of the discord server
 @client.command()
 async def cmds(ctx):
     await ctx.send("Work in progress")
 
+# Finds the price of the ticker symbol that was entered as an argument
+# If the ticker symbol does not exist, nothing happens
 @client.command()
 async def price(ctx, arg):
     try:
@@ -21,6 +25,7 @@ async def price(ctx, arg):
     except:
         pass
 
+# Lists either the gaining or losing stocks depending on the argument that was passed in
 @client.command()
 async def view(ctx, arg):
     if(arg == "gainers"):
@@ -29,6 +34,7 @@ async def view(ctx, arg):
         output = "**Top 10 Gaining Stocks of the Day**\n\n" 
         i = 1
 
+        # Formats the name, symbol, and percent change of the stock for each stock in the list
         for gainer in gainers:
             name = gainer[0]
             symbol = gainer[1]
@@ -44,6 +50,7 @@ async def view(ctx, arg):
         output = "**Top 10 Losing Stocks of the Day**\n\n" 
         i = 1
         
+        # Formats the name, symbol, and percent change of the stock for each stock in the list
         for loser in losers:
             name = loser[0]
             symbol = loser[1]
